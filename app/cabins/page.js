8f6bf7295d90f41217f8a5083d86976324409852
Keeratipong <keeratipong.b@gmail.com>
2025-04-24 15:40:39 +0700

    lecture 433: organize project folder

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,19 +1,9 @@
 import Counter from "@/components/Counter";
 
 export default async function cabinPage() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-  console.log(data);
-
   return (
     <div>
       <h1>Cabin Page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter users={data} />
     </div>
   );
 }
