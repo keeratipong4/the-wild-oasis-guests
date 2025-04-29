5fc17cd1415f9c210dec0b966b9569a72224485f
Keeratipong <keeratipong.b@gmail.com>
2025-04-29 16:38:13 +0700

    lecture 439: Building the Home Page

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,10 @@
 import Header from "@/components/Header";
-import Logo from "@/components/Logo";
-import Navigation from "@/components/Navigation";
 import "@/styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   title: {
@@ -14,10 +17,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="flex flex-col bg-primary-900 text-primary-100 min-h-screen">
-        <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="mx-auto max-w-7xl">{children}</main>
+      <body
+        className={`${josefin.className} antialiased flex flex-col bg-primary-900 text-primary-100  `}>
+        <div className="min-h-screen relative">
+          <Header />
+
+          <div className="flex-1 px-8 py-12">
+            <main className="mx-auto max-w-7xl">{children}</main>
+          </div>
         </div>
         <footer className="bg-primary-800 text-center py-5">
           <p>&copy;2025 Copyright by The Wild Oasis</p>
